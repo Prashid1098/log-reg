8867b69b6e656509c7f54e44f745a64f73cc027a
Prashid1098 <prashid1098@gmail.com>
2020-06-29 09:21:19 -0700

    commit

---
+++ prashid/diary.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="diary.css">
+        <title>Digital Diary</title>
+        </head>
+        <body>
+            <nav>
+                <a href="about.html">ABOUT </a>.
+                <a href="contact.php">CONTACT </a>
+                <a href="login1.php">LOGIN </a>
+                <a href="registernow.php">REGISTER </a>
+        </nav>
+            <div class="heading">
+                <h1 class="h">WELCOME TO DIGITAL DIARY</h1>
+            </div>
+        </body>
+    </head>
+</html>
---
+++ prashid/diarypage.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="pagestyle.css">
+<title>Diary Page</title>
+</head>
+<body>
+<header>
+
+<div class="topnav">
+<a class="active" href="diary.php">HOME</a>
+<a href="about.html">ABOUT</a>
+<a href="contact.php">CONTACT</a>
+<br>
+<div class="search-container">
+<form>
+<input type="text" placeholder="Search..." name="search">
+<button type="submit">Search<i class="fa fa-search"></i></button>
+</form>
+</div>
+</div>
+<details>
+<summary><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MY ENTRIES:</b><summary>
+<p>&nbsp;&nbsp;Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type= "date"></p><br><br>
+</details>
+<h1 id="heading1"><u>TODAY'S ENTRY</u></h1><br>
+<form>
+<p>
+&nbsp;&nbsp;&nbsp;&nbsp;<textarea id="textarea" cols="152">Start typing...</textarea><br>
+</p>
+&nbsp;&nbsp;&nbsp;&nbsp;<input id="submit" type="submit" value="submit"><br><br>
+
+</form>
+</header>
+
+
+
+
+</body>
+</html>
