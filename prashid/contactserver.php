ead148880bd4a34ea14781d038b94f1287f3abe1
Prashid1098 <prashid1098@gmail.com>
2020-07-12 23:57:00 -0700

    commit

---
+++ prashid/contactserver.php
@@ -0,0 +1,19 @@
+<?php
+$firstname="";
+$lastname="";
+$country="";
+$subject="";
+
+$db1 = mysqli_connect('localhost','root','','diary1');
+if(isset($_POST['submit'])){
+    $firstname=($_POST['firstname']);
+    $lastname=($_POST['lastname']);
+    $country=($_POST['country']);
+    $subject=($_POST['subject']);
+
+$sql1 = "INSERT INTO contacts (firstname,lastname,country,subject)
+                    VALUES ('$firstname','$lastname','$country','$subject')";
+                    mysqli_query($db1,$sql1);
+                    header('location:diary.php');
+}
+?>
